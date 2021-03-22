bcd481995dffdb76d1562f00b7ba61477174b32a
J <james.hyojae.lee@gmail.com>
2021-03-21 20:05:36 -0700

    first commit

---
+++ email.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+product_info = pd.read_excel('C:/Users/james/Desktop/Projects/EarthOne/ATCC_product_info/Data/strain_info.xlsx')
+strain = product_info[['Strain']]
+
+
